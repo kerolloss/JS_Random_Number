b12c4f2a03b853ca71c9e1859f57ca54ae440f13
kerollos <kerolloskhair@gmail.com>
2023-07-15 02:44:41 +0300

    project done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var userNames = [
 "― Oscar Wilde",
-"― Marilyn Monroe",
 "― Albert Einstein",
 "― Frank Zappa",
 "― Marcus Tullius Cicero",
@@ -9,7 +8,6 @@ var userNames = [
 ];
 var text = [
 "Be yourself; everyone else is already taken.",
-"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
 "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
 "So many books, so little time.",
 "A room without books is like a body without a soul.",

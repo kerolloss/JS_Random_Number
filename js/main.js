555a9c51256f56034bad35b8c67784217277b5c9
kerollos <kerolloskhair@gmail.com>
2023-07-15 02:22:09 +0300

    project done

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var userNames = [
+"― Oscar Wilde",
+"― Marilyn Monroe",
+"― Albert Einstein",
+"― Frank Zappa",
+"― Marcus Tullius Cicero",
+"― Dr. Seuss",
+"― Mae West"
+];
+var text = [
+"Be yourself; everyone else is already taken.",
+"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+"So many books, so little time.",
+"A room without books is like a body without a soul.",
+"You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+"You only live once, but if you do it right, once is enough."
+];
+document.getElementById("quote").innerHTML = "Hello";
+
+function randomNumber(){
+    var num = Math.floor( Math.random() * userNames.length );
+    
+    document.getElementById("quote").innerHTML = text[num];
+    document.getElementById("author").innerHTML = userNames[num];
+}
+
